d2b43df21258db15935967797f7565776f397cf9
Krixtalx <jocoara1213@gmail.com>
2023-05-09 17:16:23 +0200

    Pr3 y ejercicios de clase

--- PracticasOpenMPI/Exec.ps1
+++ PracticasOpenMPI/Exec.ps1
@@ -1,2 +1,2 @@
-mpiexec -n 4 x64\Debug\PracticasOpenMPI.exe
+mpiexec -n 10 x64\Debug\PracticasOpenMPI.exe
 pause
--- PracticasOpenMPI/PracticasOpenMPI/Main.cpp
+++ PracticasOpenMPI/PracticasOpenMPI/Main.cpp
@@ -1,5 +1,8 @@
 #include<mpi.h> 
 #include <iostream>
+#include <thread>
+#include <chrono> 
+#include <random>
 
 void Pr1(int argc, char* argv[]) {
 	int _processId, _numProcs;
@@ -50,8 +53,120 @@ void Pr2(int argc, char* argv[]) {
 	MPI_Finalize(); //Finalización OpenMPI
 }
 
+void Pr3(int argc, char* argv[]) {
+	int _processId, _numProcs;
+	unsigned _numPoints = 10000000; //nº de puntos a lanzar.
+	unsigned _counter = 0; //nº de puntos en el interior de la circuferencia. 
+
+	MPI_Init(&argc, &argv); //Inicialización OpenMPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &_processId);  // ID del proceso actual
+	MPI_Comm_size(MPI_COMM_WORLD, &_numProcs);      // Nº de procesos
+
+	//Generador de números aleatorios. Se utiliza una distribución uniforme.
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(-1, 1);
+
+	double _x, _y, _squareDistance;
+	for (size_t i = 0; i < _numPoints; i++) {
+		_x = dis(gen);
+		_y = dis(gen);
+		_squareDistance = _x * _x + _y * _y; //Dado que el radio de la circuferencia es 1, no es necesario hacer la raiz cuadrada.
+		if (_squareDistance <= 1)
+			_counter++;
+	}
+	double _q = (double)_counter / (double)_numPoints; //Hacemos casteo a double para evitar división de enteros.
+
+	//Creamos un buffer usando memoria dinámica para hacerlo adaptativo al nº de procesos.
+	double* _buffer = new double[_numProcs];
+	for (size_t i = 0; i < _numProcs; i++) {
+		_buffer[i] = 0;
+	}
+	//Todos los procesos envian sus resultados al proceso 0, quien realizará la agregación.
+	MPI_Gather(&_q, 1, MPI_DOUBLE, _buffer, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	//Se agregan los resultados de cada uno de los procesos y se obtiene el resultado final. 
+	if (_processId == 0) {
+		_q=0;
+		for (size_t i = 0; i < _numProcs; i++) {
+			_q+=_buffer[i];
+		}
+		_q*=4/(double)_numProcs;
+		std::cout<<"Aproximación de Pi: "<<_q<<std::endl;
+	}
+
+	MPI_Finalize(); //Finalización OpenMPI
+}
+
+void EjercicioBarrier(int argc, char* argv[]) {
+	int _processId, _numProcs;
+
+	MPI_Init(&argc, &argv); //Inicialización OpenMPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &_processId);  // ID del proceso actual
+	MPI_Comm_size(MPI_COMM_WORLD, &_numProcs);      // Nº de procesos
+
+	for (size_t i = 0; i < 2; i++) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(_processId));
+		std::cout << "SIN BARRIER. Soy el proceso " << _processId << ", Hola" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(_processId));
+		std::cout << "SIN BARRIER. Soy el proceso " << _processId << ", Adios" << std::endl;
+	}
+
+	for (size_t i = 0; i < 2; i++) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(_processId));
+		MPI_Barrier(MPI_COMM_WORLD);
+		std::cout << "CON BARRIER. Soy el proceso " << _processId << ", Hola" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(_processId));
+		MPI_Barrier(MPI_COMM_WORLD);
+		std::cout << "CON BARRIER. Soy el proceso " << _processId << ", Adios" << std::endl;
+	}
+
+	MPI_Finalize(); //Finalización OpenMPI
+}
+
+void EjercicioBroadcast(int argc, char* argv[]) {
+	int _processId, _numProcs;
+	int n = 0;
+
+	MPI_Init(&argc, &argv); //Inicialización OpenMPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &_processId);  // ID del proceso actual
+	MPI_Comm_size(MPI_COMM_WORLD, &_numProcs);      // Nº de procesos
+
+	if (_processId == 0) {
+		n = 100;
+		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	} else {
+		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		std::cout << "Soy el proceso " << _processId << " y recibo " << n << std::endl;
+	}
+
+	MPI_Finalize(); //Finalización OpenMPI
+}
+
+void EjercicioGather(int argc, char* argv[]) {
+	int _processId, _numProcs;
+	int n = 0;
+
+	MPI_Init(&argc, &argv); //Inicialización OpenMPI
+	MPI_Comm_rank(MPI_COMM_WORLD, &_processId);  // ID del proceso actual
+	MPI_Comm_size(MPI_COMM_WORLD, &_numProcs);      // Nº de procesos
+	int* _buffer = new int[_numProcs];
+	for (size_t i = 0; i < _numProcs; i++) {
+		_buffer[i] = 0;
+	}
+	MPI_Gather(&_processId, 1, MPI_INT, _buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	std::cout << "Soy el proceso " << _processId << " y recibo ";
+	for (size_t i = 0; i < _numProcs; i++) {
+		std::cout << _buffer[i] << ", ";
+	}
+	std::cout << std::endl;
+	delete[] _buffer;
+	MPI_Finalize(); //Finalización OpenMPI
+}
+
 int main(int argc, char* argv[]) {
-	Pr2(argc, argv);
+	Pr3(argc, argv);
 
 	return 0;
 }
